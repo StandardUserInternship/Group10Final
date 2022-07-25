8fd98a8399bb92038a21e48d371bba03bc5a2865
Darrian <d.harmon@standarduserllc.com>
2022-07-25 12:05:39 -0500

    updates from today

--- App/views.py
+++ App/views.py
@@ -1,7 +1,7 @@
-from app import app
-from flask import render_template, redirect, url_for 
+from flask import app
+from flask import render_template#, redirect, url_for 
 from forms import LoginForm
-from flask_login import logout_user 
+#from flask_login import logout_user
 
 
 @app.route('/')
@@ -14,20 +14,24 @@ def index():
  
 @app.route("/content")
 def content():
-    content = MyContent()
+    content = content()
     # if content.validate_on_submit():
     #     return redirect(url_for("content"))
     return render_template("content.html", title="Content", content=content)
 
 @app.route("/login")#, methods=["GET", "POST"])
 def login():
+
     form = LoginForm()
     return render_template("Login.html", title="Sign In", form=form)
+@app.route("titanicGraph")
+def test():
+    return render_template("titanicGraph.html", title="Titanic Graph")
 
-@app.route("/logout")
-def logout():
-    logout_user()
-    return render_template("index.html", title="Home", user=user)
+# @app.route("/logout")
+# def logout():
+#     form = LogoutForm()
+#     return render_template("index.html", title="Sign Out", form=form)
 
 
 
