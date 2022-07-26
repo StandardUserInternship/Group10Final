0ad4d4e3f1809acd514a65a930e390ad720677f4
Kelly <Kellyvypham@gmail.com>
2022-07-25 19:09:03 -0500

    New changes work in progress

--- App/testing.py
+++ App/testing.py
@@ -2,10 +2,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def testingFunc():
-    df = pd.read_csv("titanic.csv")
-    df = df[['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+df = pd.read_csv("titanic.csv")
+df = df[['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
         'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
-    cor = df.corr()
-    print(cor)
-    sns.heatmap(data = cor)
+cor = df.corr()
+sns.heatmap(cor, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag')
+plt.show()
+plt.savefig('heatmap.png')
--- App/views.py
+++ App/views.py
@@ -1,6 +1,10 @@
-
-from flask import render_template
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, show
+from flask import render_template, jsonify
 from app import app
+import base64
 @app.route('/')
 @app.route('/index')
 #It is using this here instead of in the __innit__.py!
@@ -32,6 +36,23 @@ def monthGraph():
 def testing():
     return render_template("testing.html")
 
+@app.route('/test2')
+def test2():
+    plot(1)  
+    show(block=False)
+    df = pd.read_csv("titanic.csv")
+    df = df[['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+        'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']]
+    cor = df.corr()
+    sns.heatmap(cor, annot=True, vmax=1, vmin=-1, center=0, cmap='vlag')
+    plt.savefig('heatmap.png')
+
+    data = {}
+    with open('heatmap.png', mode='rb') as file:
+        img = file.read()
+    data['img'] = base64.encodebytes(img).decode()
+    
+    return jsonify(data['img'])
 
     
 
